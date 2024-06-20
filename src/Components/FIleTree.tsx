d3a0e9e892028ffa4752dde544b330ed8abdb2db
Gustave HERVE <gustave.herve@epita.fr>
2024-06-20 14:40:26 +0200

    feat(filetree): added example architecture fetch in FIleTree.tsx component

--- src/Components/FIleTree.tsx
+++ src/Components/FIleTree.tsx
@@ -1,12 +1,40 @@
 import FolderTree, { testData } from 'react-folder-tree';
 import 'react-folder-tree/dist/style.css';
+import {useEffect, useState} from "react";
 
 const BasicTree = () => {
     const onTreeStateChange = (state: any, event: any) => console.log(state, event);
+    const [fileTree, setFileTree] = useState(testData);
 
+    useEffect( () => {
+        const fetchArchitecture = async () => {
+            try {
+                console.log("Sending message to backend:", "./");
+                const response = await fetch('http://localhost:8080/api/architecture', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({path: "./"})
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setFileTree(data);
+                console.log("Received response from backend:", data);
+                return data;
+
+            } catch (error) {
+                console.error('Error during fetch:', error);
+                return testData;
+            }
+        }
+        fetchArchitecture().then(r => setFileTree(r));
+    }, []);
     return (
         <FolderTree
-            data={ testData }
+            data={ fileTree }
             onChange={ onTreeStateChange }
         />
     );
