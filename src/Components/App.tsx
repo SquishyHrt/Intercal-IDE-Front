41b61fc0b5e0ce1ccf741b8e31194d14108aa7d6
Lucas Besnard <lucas.besnard@epita.fr>
2024-06-20 16:49:34 +0200

    feat(code-editor): change to monaco editor, completion and syntax highlight works

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,37 +2,38 @@ import '../App.css'
 import BasicTree from "Components/FIleTree.tsx";
 import AceEdit from "Components/AceEditor.tsx";
 import Chat from "Components/Chat.tsx";
+import MonacoEditor from './MonacoEditor';
 
 const App = () => {
     return (
         <body>
-        <div className="container">
-            <div className="top-box">
-                <div className="top-part">
-                    <div className="buttons">
-                        <button className="button-1"></button>
-                        <button className="button-2"></button>
-                        <button className="button-3"></button>
-                        <button className="button-4"></button>
+            <div className="container">
+                <div className="top-box">
+                    <div className="top-part">
+                        <div className="buttons">
+                            <button className="button-1"></button>
+                            <button className="button-2"></button>
+                            <button className="button-3"></button>
+                            <button className="button-4"></button>
+                        </div>
+                    </div>
+                    <div className="bottom-part"></div>
+                </div>
+                <div className="bottom-container">
+                    <div className="bottom-box">
+                        <BasicTree />
+                    </div>
+                    <div className="bottom-box" id="editor-box">
+                        <MonacoEditor />
+                    </div>
+                    <div className="bottom-box">
+                        <Chat />
+                    </div>
+                    <div className="bottom-box">
+                        <button className="bleachers-box"></button>
                     </div>
                 </div>
-                <div className="bottom-part"></div>
             </div>
-            <div className="bottom-container">
-                <div className="bottom-box">
-                    <BasicTree/>
-                </div>
-                <div className="bottom-box" id="editor-box">
-                    <AceEdit/>
-                </div>
-                <div className="bottom-box">
-                    <Chat/>
-                </div>
-                <div className="bottom-box">
-                    <button className="bleachers-box"></button>
-                </div>
-            </div>
-        </div>
         </body>
     );
 }
---
+++ src/Components/MonacoEditor.tsx
@@ -0,0 +1,78 @@
+import React, { useRef, useEffect, useState } from 'react';
+import * as monaco from 'monaco-editor';
+
+const MonacoEditor = () => {
+    const editorRef = useRef(null);
+
+    useEffect(() => {
+        // Define the INTERCAL language
+        monaco.languages.register({ id: 'intercal' });
+
+        monaco.languages.setMonarchTokensProvider('intercal', {
+            tokenizer: {
+                root: [
+                    [/\bDO\b|\bPLEASE\b|\bIGNORE\b|\bNEXT\b|\bABSTAIN\b|\bFORGET\b|\bREMEMBER\b|\bREINSTATE\b|\bRESUME\b|\bGIVE UP\b|\bCOMPUTE\b|\bCOME FROM\b|\bWRITE IN\b|\bREAD OUT\b|\bGIVE UP\b/, 'keyword'],
+                    [/\b(?:0|1|2|3|4|5|6|7|8|9)+\b/, 'number'],
+                    [/\/\/.*/, 'comment'],
+                    [/"([^"\\]|\\.)*$/, 'string.invalid'],
+                    [/\(/, 'delimiter.parenthesis'],
+                    [/\)/, 'delimiter.parenthesis'],
+                    [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+                ],
+                string: [
+                    [/[^\\"]+/, 'string'],
+                    [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }],
+                ],
+            },
+        });
+
+        monaco.languages.setLanguageConfiguration('intercal', {
+            comments: {
+                lineComment: '//',
+            },
+            brackets: [
+                ['{', '}'],
+                ['[', ']'],
+                ['(', ')'],
+            ],
+            autoClosingPairs: [
+                { open: '{', close: '}' },
+                { open: '[', close: ']' },
+                { open: '(', close: ')' },
+                { open: '"', close: '"' },
+            ],
+        });
+
+        // Initialize the Monaco Editor
+        editorRef.current = monaco.editor.create(document.getElementById('editor'), {
+            value: '',
+            language: 'intercal',
+            theme: 'vs-dark',
+            automaticLayout: true,
+            width: '100%',
+            height: '100%',
+        });
+
+
+        const completion = monaco.languages.registerCompletionItemProvider('intercal', {
+            provideCompletionItems: () => {
+                const keywords = ['DO', 'PLEASE', 'IGNORE', 'NEXT', 'ABSTAIN', 'FORGET', 'REMEMBER', 'REINSTATE', 'RESUME', 'GIVE UP', 'COMPUTE', 'COME FROM', 'WRITE IN', 'READ OUT', 'GIVE UP'];
+                const suggestions = keywords.map(keyword => ({
+                    label: keyword,
+                    kind: monaco.languages.CompletionItemKind.Keyword,
+                    insertText: keyword,
+                }));
+                return { suggestions: suggestions };
+            },
+        });
+
+        return () => {
+            completion.dispose();
+            editorRef.current.dispose();
+        };
+    }, []);
+
+    return <div id="editor" style={{ height: '100%', width: '95%' }} />;
+};
+
+export default MonacoEditor;
