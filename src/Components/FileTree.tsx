ea514df486a462763b546ed8d3696c06a91ecfdb
Lucas Besnard <lucas.besnard@epita.fr>
2024-06-24 18:00:12 +0200

    add logging for tree action

--- src/Components/FileTree.tsx
+++ src/Components/FileTree.tsx
@@ -1,10 +1,48 @@
-import FolderTree, {testData} from 'react-folder-tree';
+import FolderTree, { testData } from 'react-folder-tree';
 import 'react-folder-tree/dist/style.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+
+function getNode(path: number[], fileTree: any) {
+    let tmp: any = fileTree.children;
+    for (const i of path) {
+        tmp = tmp[i].children ? tmp[i].children : tmp[i];
+    }
+    return tmp;
+}
 
 const BasicTree = () => {
-    const onTreeStateChange = (state: any, event: any) => console.log(state, event);
     const [fileTree, setFileTree] = useState(testData);
+    const onTreeStateChange = (state: any, event: any) => {
+
+        console.log(state, event);
+
+        if (event.type == "deleteNode") {
+            const prevTree = JSON.parse(JSON.stringify(fileTree));
+            const { absolutePath } = getNode(event.path, prevTree);
+
+            console.log("Deleting node at:", absolutePath)
+        }
+        else if (event.type == "renameNode") {
+            const prevTree = JSON.parse(JSON.stringify(fileTree));
+            const { absolutePath } = getNode(event.path, prevTree);
+            // newName doesn't contains the full path
+            const newName = event.params[0];
+
+            console.log("Renaming node. Previous path:", absolutePath, ". New name:", newName);
+        }
+        else if (event.type == "addNode") {
+            // Problem because the added node has no absolutePath attribute
+            // We need to recompose the path using event.path
+            const { name } = getNode(event.path, state)[0];
+
+            if (event.params[0] == false) {
+                console.log("Creating file called:", name);
+            }
+            else {
+                console.log("Creating folder called:", name);
+            }
+        }
+    };
 
     useEffect(() => {
         const fetchArchitecture = async () => {
@@ -12,8 +50,8 @@ const BasicTree = () => {
                 console.log("Sending message to backend:", "./");
                 const response = await fetch('http://localhost:8080/api/architecture', {
                     method: 'POST',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({path: "./"})
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ path: "/home/lb/EPITA/PING/samuelIDE/my-app" })
                 });
 
                 if (!response.ok) {
@@ -32,10 +70,21 @@ const BasicTree = () => {
         }
         fetchArchitecture().then(r => setFileTree(r));
     }, []);
+
+    const onNameClick = ({ defaultOnClick, nodeData }) => {
+        defaultOnClick();
+
+        const { absolutePath } = nodeData;
+
+        console.log('File selected: ', absolutePath);
+    };
+
     return (
         <FolderTree
             data={fileTree}
             onChange={onTreeStateChange}
+            onNameClick={onNameClick}
+            showCheckbox={false}
         />
     );
 };
